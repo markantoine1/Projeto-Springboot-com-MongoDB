28cdfab7ac2e0fbf72cd9e39de2b65d91c28bbce
markantoine1 <markantoine1718@gmail.com>
2022-12-16 17:29:32 -0300

    Obtendo um usuário por ID

--- mongoproject/src/main/java/com/markantoine/mongoproject/services/UserService.java
+++ mongoproject/src/main/java/com/markantoine/mongoproject/services/UserService.java
@@ -1,12 +1,14 @@
 package com.markantoine.mongoproject.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.markantoine.mongoproject.domain.User;
 import com.markantoine.mongoproject.repositories.UserRepository;
+import com.markantoine.mongoproject.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id){
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
