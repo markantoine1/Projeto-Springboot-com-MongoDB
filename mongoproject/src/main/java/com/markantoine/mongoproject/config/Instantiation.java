5f6c0c600ca1775e0012cfb00936a837739dd695
markantoine1 <markantoine1718@gmail.com>
2022-12-16 16:25:33 -0300

    Operação de instanciação da base de dados

---
+++ mongoproject/src/main/java/com/markantoine/mongoproject/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.markantoine.mongoproject.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.markantoine.mongoproject.domain.User;
+import com.markantoine.mongoproject.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository repository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		repository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		repository.saveAll(Arrays.asList(maria,alex,bob));
+		
+	}
+
+}
